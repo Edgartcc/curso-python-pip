7c70d31f073ed82cabf52430c1762ad5aa2baf8e
Edgar Tipanguano <edgar.tipanguano@outlook.com>
2024-05-01 21:24:25 -0500

    Adding a lot of things

--- app/main.py
+++ app/main.py
@@ -1,49 +1,46 @@
-import utils # estamos llamando el archivo donde esta la funcion get_population y este es el modulo que hemos creado
-
+import utils
 import read_csv
 import charts
-# esta es la lista con diccionario:
-'''
-data = [
-  {
-    'Country': 'Colombia',
-    'Population': 500
-  },
-  {
-    'Country': 'Bolivia',
-    'Population': 300
-  }
-]
-'''
-#definimos una funcion para que solo funcione si la pedimos, o sea si escribimos main.run()
+import pandas as pd
+
 def run():
-  #keys, values = utils.get_population()
-  #print(keys, values)
+  
+  #Código para generar el pie chart sin usar PANDAS
+  '''
   data = read_csv.read_csv('data.csv')
-  # tambien podemos filtrar la informacion para el reto 2
-  # data = list(filter(lambda item : item['Continent'] == 'South America', data))
-  countries = list(map(lambda x: x['Country/Territory'], data))
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+
+  countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
+  '''
+  # #Código equivalente usando PANDAS
+  
+  # df(dataframe) 
+  df = pd.read_csv("data.csv")  # Nos ahorramos el método creado read_csv.py
+  df = df[df['Continent'] == 'South America']  
+  # Equivalente a -> data = list(filter(lambda item : item['Continent'] == 'South America',data))
+  countries = df['Country'].values
+  # Equivalente a -> countries = list(map(lambda x: x['Country'], data))
+  percentages = df['World Population Percentage'].values
+  # Equivalente a -> percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
 
+
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
-  
-  
-  # le enviamos dos parametros, data, que es la lista donde esta la informacion y el segundo es el pais que deseamos obtener.
-  # result = utils.population_by_country(data, 'Colombia')
-  
-  # pero ahora usando input, en vez de 'Colombia', ponemos country
+  print(country)
+
   result = utils.population_by_country(data, country)
+  
+  
+  
 
-  if len(result) > 0: # o sea que encontro algun pais
+  if len(result) > 0:
     country = result[0]
+    print(country)
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(country['Country/Territory'], labels, values)
-
-  
-  
-
-# pero ahora main.py ya no sirve desde la terminal(shell) puedes usar if. Este if le dice al archivo main.py que si es ejecutado desde la terminal ejecute run, pero no desde otro archivo
+    charts.generate_bar_chart(country['Country'], labels, values)
 
 if __name__ == '__main__':
   run()
